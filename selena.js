fa742ee15a23b10a445eb9b59df5b884394b0dc6
sabler <rob_sable@gap.com>
2019-11-23 00:11:25 -0800

    basic search working

--- selena.js
+++ selena.js
@@ -2,18 +2,24 @@ const webdriver = require('selenium-webdriver');
 const fs = require ('fs');
 
 const babyDriver = new webdriver.Builder();
+var driver = null;
 
 async function initBrowser() {
-    let driver = babyDriver.forBrowser('safari').build(); // Build a new web session
+    driver = babyDriver.forBrowser('safari').build(); // Build a new web session
     let navigateToPage = await driver.get('http://www.tfwiki.net'); // Open a URL
-
-
 }
 
 
 
+async function runASearch(str) {
+    let searchForm = await driver.findElement(webdriver.By.id('searchform'));
+    let searchForBot = await driver.findElement(webdriver.By.id('searchInput')).sendKeys(str);
+    let searchForGoButton = await driver.findElement(webdriver.By.id('searchGoButton'));
 
+    searchForGoButton.sendKeys(webdriver.Key.ENTER);
+    let searchForNull = await driver.wait(webdriver.until.elementLocated(webdriver.By.linkText('Cyberverse!!'),50000));
+}
 
 initBrowser().then(function(){
-    console.log("Finished!");
+    runASearch('megatron');
 });
